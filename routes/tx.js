4f7e7dd1c64e832ff82f9f1699459521e5bd7e04
Aleksandr Shepelev <a.shepelev@innosoft.pro>
2018-10-16 22:23:44 +0300

    Add cancellation game

--- routes/tx.js
+++ routes/tx.js
@@ -155,6 +155,18 @@ router.get('/:tx', function(req, res, next) {
         tx.recipient = decoded.inputs[0].toString(16);
         tx.coinsTransferred = decoded.inputs[1].toString(10);
         break;
+    case "cancellationGame":
+      tx.operation = "Cancel game";
+      tx.gamers = ""
+      for (var i=0; i<decoded.inputs[1].length; i++) {
+        tx.gamers += decoded.inputs[1][i].toString(16);
+        if (i !== decoded.inputs[1].length - 1) {
+          tx.gamers += ", "
+        }
+      }
+      tx.internalGameId = decoded.inputs[0];
+      break;
+
     }
 
     tx.decoded = JSON.stringify(decoded, null, 2);
