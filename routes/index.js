597d73ae144a4fa7db7c8bf4c8f8a5367ce289bf
Aleksandr Shepelev <a.shepelev@innosoft.pro>
2018-10-24 19:37:47 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,18 @@ var async = require('async');
 var Web3 = require('web3');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   var config = req.app.get('config');
   var web3 = new Web3();
   web3.setProvider(config.provider);
 
   async.waterfall([
-    function(callback) {
-      web3.eth.getBlock("latest", false, function(err, result) {
+    function (callback) {
+      web3.eth.getBlock("latest", false, function (err, result) {
         callback(err, result);
       });
-    }, function(lastBlock, callback) {
+    }, function (lastBlock, callback) {
       var blocks = [];
 
       var blockCount = 10;
@@ -25,81 +25,55 @@ router.get('/', function(req, res, next) {
         blockCount = lastBlock.number + 1;
       }
 
-      async.times(blockCount, function(n, next) {
-        web3.eth.getBlock(lastBlock.number - n, true, function(err, block) {
+      async.times(blockCount, function (n, next) {
+        web3.eth.getBlock(lastBlock.number - n, true, function (err, block) {
           next(err, block);
         });
-      }, function(err, blocks) {
+      }, function (err, blocks) {
         callback(err, blocks);
       });
     }
-  ], function(err, blocks) {
+  ], function (err, blocks) {
     if (err) {
       return next(err);
     }
 
     var txs = [];
-    blocks.forEach(function(block) {
-      block.transactions.forEach(function(tx) {
+    blocks.forEach(function (block) {
+      block.transactions.forEach(function (tx) {
         if (txs.length === 10) {
           return;
         }
         txs.push(tx);
       });
     });
-    res.render('index', { blocks: blocks, txs: txs });
+    res.render('index', {blocks: blocks, txs: txs});
   });
 
 });
 
 
-router.get('/txsblocks', function(req, res, next) {
+router.get('/txsblocks', function (req, res, next) {
 
   var config = req.app.get('config');
   var web3 = new Web3();
   web3.setProvider(config.provider);
 
   async.waterfall([
-    function(callback) {
-      web3.eth.getBlock("latest", false, function(err, result) {
-        callback(err, result);
+    function (callback) {
+      blockLog.find({}).sort({number: -1}).exec(function (err, blocks) {
+        callback(err, blocks)
       });
-    }, function(lastBlock, callback) {
-      var blockCount = 100;
-
-      if (lastBlock.number - blockCount < 0) {
-        blockCount = lastBlock.number + 1;
-      }
-
-      var counter = 0;
-
-      var numOfBlocksWithTxs = 0;
-
-      var blocks = [];
-      async.whilst(
-        function () {
-          return (!((lastBlock.number - counter < 0) || (numOfBlocksWithTxs >= blockCount)))
-        },
-        function(next) {
-          web3.eth.getBlock(lastBlock.number - counter, true, function(err, block) {
-            counter++;
-            if (block.transactions.length > 0) {
-              numOfBlocksWithTxs++;
-              blocks.push(block)
-            }
-            next(err, block);
-          });
-        },
-        function(err) {
-          callback(err, blocks);
-        });
+    },
+    function (blocks, callback) {
+      callback(blocks);
     }
-  ], function(err, blocks) {
+  ], function (err, blocks) {
     if (err) {
       return next(err);
     }
 
-    res.render('txsblocks', { blocks: blocks });
+    res.render('txsblocks', {blocks: blocks});
   });
 
 });
