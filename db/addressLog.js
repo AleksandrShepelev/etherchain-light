53479369cb35e1af54937b30b134b1d712fef537
Aleksandr Shepelev <a.shepelev@innosoft.pro>
2018-10-24 14:49:57 +0300

    Fix connection

--- db/addressLog.js
+++ db/addressLog.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-var db = mongoose.createConnection('localhost', 'cybe');
+var db = mongoose.connect('mongodb://localhost:27017/cybe');
 
 const AddressLogSchema = mongoose.Schema(
   {
